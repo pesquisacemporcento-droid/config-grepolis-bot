188a1902147d90fbd47ab12e77672468aaacdf3f
pesquisacemporcento-droid <pesquisacemporcento@gmail.com>
2025-12-06 16:40:57 -0300

    Update save-config.ts

--- api/save-config.ts
+++ api/save-config.ts
@@ -1,4 +1,4 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { Octokit } from '@octokit/rest';
 
 const repoOwner = process.env.GITHUB_OWNER!;
